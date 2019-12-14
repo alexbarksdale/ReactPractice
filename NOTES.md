8c92760b077f1af094b035b9069f66d3b93263d8
alexbarksdale <ale.barksdale@gmail.com>
2019-12-14 11:55:10 -0800

    Props intro

---
+++ NOTES.md
@@ -0,0 +1,14 @@
+# JSX
+
+HTML <div style="background-color: red;></div>
+JSX <div style={{ backgroundColor: 'red' }}></div>
+∆ The first '{}' references a javascript variables inside JSX
+∆ The second '{{}}' indicates a javascript obj.
+
+# Components
+
+## Props
+
+- Props is short for 'Properties'
+- System for passing data from a parent component to a child component
+- Goal is to customize or config a child component
--- components/src/index.js
+++ components/src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 import faker from 'faker';
 
 const App = () => {
-    return <div className='ui container comments'></div>;
+    return (
+        <div className='ui container comments'>
+            <CommentDetail
+                author='Alex'
+                timeAgo='Today at 4:45pm'
+                commentMsg='Super cool'
+                profileAvatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author='Abby'
+                timeAgo='Today at 2:45pm'
+                commentMsg='Wowzah'
+                profileAvatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author='Vick'
+                timeAgo='Today at 6:45pm'
+                commentMsg='Wicked'
+                profileAvatar={faker.image.avatar()}
+            />
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
