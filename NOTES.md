4b0e31c2912dc5a981c372c3ca7259ad5effba6a
alexbarksdale <ale.barksdale@gmail.com>
2019-12-14 13:00:37 -0800

    Final basic component and prop practice

--- NOTES.md
+++ NOTES.md
@@ -12,3 +12,13 @@ JSX <div style={{ backgroundColor: 'red' }}></div>
 - Props is short for 'Properties'
 - System for passing data from a parent component to a child component
 - Goal is to customize or config a child component
+
+# Class Components
+
+Benefits:
+
+- Organization
+- Can use 'state' --- another react system
+  - Easier to handle user input
+- Understands lifecycle events
+  - Easier to do things when the app first starts
--- components/src/index.js
+++ components/src/index.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className='ui container comments'>
-            <CommentDetail
-                author='Alex'
-                timeAgo='Today at 4:45pm'
-                commentMsg='Super cool'
-                profileAvatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author='Abby'
-                timeAgo='Today at 2:45pm'
-                commentMsg='Wowzah'
-                profileAvatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author='Vick'
-                timeAgo='Today at 6:45pm'
-                commentMsg='Wicked'
-                profileAvatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                {/*NOTE: Must pass props.children in approval card*/}
+                <CommentDetail
+                    author='Alex'
+                    timeAgo='Today at 4:45pm'
+                    commentMsg='Super cool'
+                    profileAvatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Abby'
+                    timeAgo='Today at 2:45pm'
+                    commentMsg='Wowzah'
+                    profileAvatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author='Vick'
+                    timeAgo='Today at 6:45pm'
+                    commentMsg='Wicked'
+                    profileAvatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
