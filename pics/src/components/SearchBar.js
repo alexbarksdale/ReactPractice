4aa71c62dfb8ce281018c9df68a238a78e828bdb
alexbarksdale <ale.barksdale@gmail.com>
2019-12-18 00:53:57 -0600

    Handling forms

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
+    state = { term: '' };
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(this.state.term);
     }
 
     render() {
         return (
             <div className='ui segment '>
-                <form action='' className='ui form'>
+                <form action='' className='ui form' onSubmit={this.onFormSubmit}>
                     <div className='field'>
                         <label>Image Search</label>
-                        <input type='text' onChange={this.onInputChange} />
+                        <input
+                            type='text'
+                            value={this.state.term}
+                            onChange={(e) => {
+                                this.setState({ term: e.target.value });
+                            }}
+                        />
                     </div>
                 </form>
             </div>
