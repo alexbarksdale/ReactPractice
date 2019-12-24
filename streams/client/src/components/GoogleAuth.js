728db68ca9e3a730e47242a1f111b020e4dc0e72
alexbarksdale <ale.barksdale@gmail.com>
2019-12-24 02:10:34 -0600

    Add | AuthBtn functionality

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -23,19 +23,27 @@ export default class GoogleAuth extends Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     };
 
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+
     renderAuthBtn() {
         if (this.state.isSignedIn === null) {
             return;
         } else if (this.state.isSignedIn) {
             return (
-                <button className='ui red google button'>
+                <button className='ui red google button' onClick={this.onSignOut}>
                     <i className='google icon' />
                     Sign Out
                 </button>
             );
         } else {
             return (
-                <button className='ui green google button'>
+                <button className='ui green google button' onClick={this.onSignIn}>
                     <i className='google icon' />
                     Sign In
                 </button>
