727efcae566814ce4ae2be5ac88d23aa191a267f
alexbarksdale <ale.barksdale@gmail.com>
2019-12-24 20:26:14 -0600

    Add | Actions for project

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
     renderError({ error, touched }) {
@@ -24,9 +26,9 @@ class StreamCreate extends Component {
         );
     };
 
-    onSubmit(formValues) {
-        console.log(formValues);
-    }
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues);
+    };
 
     render() {
         return (
@@ -65,7 +67,9 @@ const validate = (formValues) => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'STREAM_CREATE',
     validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
